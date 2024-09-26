25472fcb2cba2f589a5b7f3e1842532f6b519a4f
Darkpool645 <angel_camargo2020@outlook.com>
2024-09-26 14:43:01 -0600

    Subida del proyecto

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour{
+    public float moveSpeed = 5.0f;
+    private Rigidbody rb;
+
+    private void Start(){
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    private void Update(){
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        rb.AddForce(moveX * moveSpeed, 0, moveZ * moveSpeed, ForceMode.Acceleration);
+    }
+} 
